1c63c283d213210c8b608409cffbfacf3d4cb264
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-25 17:41:07 +0200

    ✨ Match tab content with selected tab

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var tabs = $('.features-tabs__tab')
+var tabsContent = $('.features-tabs-content__tab-container')
 tabs.click(function (e) {
   e.preventDefault()
+  var me = $(this)
   tabs.removeClass('features-tabs__tab--selected')
-  $(this).addClass('features-tabs__tab--selected')
+  me.addClass('features-tabs__tab--selected')
+  tabsContent.removeClass('features-tabs-content__tab-container--selected')
+  tabsContent
+    .filter((i, tab) => $(tab).data('id') === me.data('id'))
+    .addClass('features-tabs-content__tab-container--selected')
 })
