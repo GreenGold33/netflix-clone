39b1a7252303abf2524c6432b36979affa819cf7
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-26 11:00:58 +0200

    🔨 Use semantic elements

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 var tabs = $('#features > nav a')
-var tabsContent = $('.features-tabs-content__tab-container')
+var tabsContent = $('#features > article > section')
 tabs.click(function (e) {
   e.preventDefault()
   var me = $(this)
   tabs.removeClass('is-selected')
   me.addClass('is-selected')
-  tabsContent.removeClass('features-tabs-content__tab-container--selected')
+  tabsContent.removeClass('is-selected')
   tabsContent
     .filter((i, tab) => $(tab).data('id') === me.data('id'))
-    .addClass('features-tabs-content__tab-container--selected')
+    .addClass('is-selected')
 })
