8642edb09e3f28823a9cf555c74997b38cde5d7d
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-26 10:25:28 +0200

    🔨 Use simpler css without BEM

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-var tabs = $('.features-tabs__tab')
+var tabs = $('#features > nav a')
 var tabsContent = $('.features-tabs-content__tab-container')
 tabs.click(function (e) {
   e.preventDefault()
   var me = $(this)
-  tabs.removeClass('features-tabs__tab--selected')
-  me.addClass('features-tabs__tab--selected')
+  tabs.removeClass('is-selected')
+  me.addClass('is-selected')
   tabsContent.removeClass('features-tabs-content__tab-container--selected')
   tabsContent
     .filter((i, tab) => $(tab).data('id') === me.data('id'))
